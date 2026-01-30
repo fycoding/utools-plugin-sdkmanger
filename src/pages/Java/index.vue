f38d52db3acc0ec222a4da86213bcf66406de990
zfy <1531108847@qq.com>
2026-01-30 10:26:43 +0800

    添加java SDK 版本

--- src/pages/Java/index.vue
+++ src/pages/Java/index.vue
@@ -6,6 +6,8 @@ let sdkPath = ref('');
 const versionList = ref([
   { version: '8', downloadUrl: 'https://d10.injdk.cn/openjdk/openjdk/8/openjdk-8u41-b04-windows-i586-14_jan_2020.zip' },
   { version: '11', downloadUrl: 'https://d10.injdk.cn/openjdk/openjdk/11/openjdk-11+28_windows-x64_bin.zip' },
+  { version: '17', downloadUrl: 'https://d10.injdk.cn/openjdk/openjdk/17/openjdk-17.0.1_windows-x64_bin.zip' },
+  { version: '21', downloadUrl: 'https://d10.injdk.cn/openjdk/openjdk/21/openjdk-21.0.2_windows-x64_bin.zip' },
   { version: '25', downloadUrl: 'https://d10.injdk.cn/openjdk/openjdk/25/openjdk-25_windows-x64_bin.zip' },
 ]);
 
